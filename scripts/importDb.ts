faf489c3d9b2f2a9f40e250a9ba6da7a391693f5
artlu99 <alu99.sm@gmail.com>
2025-03-11 12:32:43 -0400

    script

---
+++ scripts/importDb.ts
@@ -0,0 +1,46 @@
+import { createClient } from '@libsql/client/web';
+import dotenv from 'dotenv';
+import fs from 'fs';
+const config = dotenv.config({ path: '.env.local' });
+
+const turso = createClient({
+	url: config.parsed?.TURSO_DATABASE_URL!,
+	authToken: config.parsed?.TURSO_AUTH_TOKEN,
+});
+
+const fullCopy = async () => {
+	await turso.execute('DROP TABLE IF EXISTS stored_data');
+
+	// apply schema from ./stored_data.sql
+	const schema = fs
+		.readFileSync('./src/lib/d1.sql', 'utf8')
+		.split('\n')
+		.join(' ');
+	if (schema) {
+		console.log(schema);
+		try {
+			await turso.execute(schema);
+			console.log('Schema applied');
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	// assume the script in `package.json` has already exported the data to ./stored_data.sql
+	const file = fs.readFileSync('./stored_data.sql', 'utf8');
+	const sql = file.split('\n');
+
+	// import it into the turso db using liqsql client
+	for (const line of sql) {
+		if (line.trim() !== '') {
+			try {
+				const result = await turso.execute(line);
+				console.log(result.lastInsertRowid);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+};
+
+fullCopy();
