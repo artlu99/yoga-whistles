f0ca7e0314684e043d97093c238a2aa157415f38
artlu99 <alu99.sm@gmail.com>
2025-04-27 15:50:27 -0400

    Pinata api deprecated

--- src/lib/hub.ts
+++ src/lib/hub.ts
@@ -2,13 +2,13 @@ import { fetcher } from 'itty-fetcher';
 import { CastObject } from './neynar-types';
 import { Env } from '../types';
 
-const endpoint = 'https://api.pinata.cloud/v3/farcaster/casts/';
+const endpoint = 'https://api.neynar.com/v2/farcaster';
 
 export const getCastByHash = async (hash: string, env: Env) => {
 	const res = await fetcher({
 		base: endpoint,
-		headers: { Authorization: `Bearer ${env.PINATA_JWT}` },
-	}).get<{ cast: CastObject }>(hash);
+		headers: { 'x-api-key': env.NEYNAR_API_KEY },
+	}).get<{ cast: CastObject }>(`/cast?identifier=${hash}&type=hash`);
 
 	if (!res.cast.hash) {
 		console.error(endpoint, JSON.stringify(res));
